e536fe9e77b8978ef0e00a7a53515074d9d0326d
Jess-dudu <jessica.dudu.chen@gmail.com>
2025-01-03 13:51:41 -0800

    Add streamlit based web service support

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # PythonForIntuitiveMath
 
-Learn and study math with Python
+Learn and study math with Python. This project started with IPython Notebook 
+to help demonstrating and understanding Math topics with interactive graphing, 
+visualization, data analysis & simulation. For better accessiblity through 
+web browser, the project is switched to utilize streamlit instead. 
---
+++ app/topics/home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Python App for Intuitive Math")
+
+st.write('''
+         The project started with IPython Notebook to help demonstrating and understanding 
+         Math topics with interactive graphing, visualization, data analysis & simulation. 
+         For better accessiblity through web browser, the project is switched to utilize 
+         streamlit instead.  
+         ''')

55a5b25624178c462635bdb96b3806da43a5b793
Jess-dudu <jessica.dudu.chen@gmail.com>
2025-08-21 08:38:05 +0000

    Update UAM topic

--- app/topics/11_physics_uam.py
+++ app/topics/11_physics_uam.py
@@ -20,21 +20,21 @@ st.write("\n")
 
 in_col1, in_col2, in_col3 = st.columns(3)
 with in_col1:
-    v0 = st.number_input("Initial velocity (m/s)", value=-0.5, step=0.25)
+    v0 = st.number_input("Initial velocity (m/s)", value=2.0, step=0.25)
 with in_col2:
     a = st.number_input("Acceleration (m/s²)", value=1.0, step=0.25)
 with in_col3:
     t_max = st.number_input("Total time (s)", min_value=1.0, max_value=10.0, value=3.0, step=1.0)
 
 st.write("\n")
-col1, col2 = st.columns([0.33, 0.67])
+col1, col2 = st.columns([0.5, 0.5])
 with col1:
     st.write("Customize the graph X/Y Axes\n")
     # op_y = st.selectbox("Y Axis", ("Position", "Velocity", "Acceleration"))
     op_y = st.radio(
         "Y Axis:",
         options=["Position", "Velocity", "Acceleration"],
-        index=0,  # Position will be pre-selected
+        index=0,  # Velocity will be pre-selected
     )
 
     # op_x = st.selectbox("X Axis", ("Time", "Position"))
@@ -69,3 +69,45 @@ with col2:
     st.pyplot(fig)
 
 
+st.subheader("Understanding UAM with Velocity-Time Graphs")
+
+st.markdown(r'''There are five variables in uniformly accelerated motion (UAM). They are 
+    initial velocity ($v_0$), final velocity ($v_t$), acceleration ($a$), time of 
+    acceleration ($t$), and displacement ($\Delta x$). The graph forms a trapezoid, 
+    where the area of shaded region is the total displacement. 
+    ''')
+
+col1, col2 = st.columns([0.5, 0.5])
+with col1:
+    st.markdown(r"- $v_0$ = " + rf"{v0} (trapezoid top)")
+    st.markdown(r"- $v_t$ = " + rf"{v[-1]} (trapezoid base)")
+    st.markdown(r"- $a$ = " + rf"{a} (velocity slope)")
+    st.markdown(r"- $t$ = " + rf"{t_max} (trapezoid height)")
+    st.markdown(r"- $\Delta x$ = " + rf"{x[-1]} (trapezoid area)")
+    st.markdown(r"Given any three variables, the trapezoid is decided and the other two variables can be calculated.")
+with col2:
+    x_arr = t
+    y_arr = v
+    fig, ax = plt.subplots(figsize=(6, 4))
+    plt.ylim(-1, 6)
+    plt.xlim(0, max(x_arr)+0.15)
+    ax.fill_between(x_arr, 0, y_arr, color='lightblue')
+    ax.plot(x_arr, y_arr, label=f"Velocity (m/s)")
+    ax.set_xlabel(f"Time (s)")
+    ax.set_ylabel(f"Velocity (m/s)")
+    ax.set_title(f"Velocity (m/s) vs. Time (s)")
+    ax.grid(True)
+    ax.legend()
+
+    st.pyplot(fig)
+
+st.subheader("Derivation of Common UAM Equations")
+
+st.markdown(r'''Starting from rest: $v_0 = 0$, trapezoid simplifies to a right angle triangle''')
+st.markdown(r'''- $v_t = a t \qquad \quad$ ($v_t$ is triangle height and $t$ is triangle base)''')
+st.markdown(r'''- $\Delta x = \frac{1}{2} v_t t = \frac{1}{2} a t^2 \quad$ ($\Delta x$ is triangle area)''')
+st.markdown(r'''General case: $v_0 \neq 0$''')
+st.markdown(r'''- $v_t = v_0 + a t \quad \Leftrightarrow \quad a = (v_t - v_0)/t$''')
+st.markdown(r'''- $\Delta x = v_0 t + \frac{1}{2} a t^2 \quad$ (sum of rectangle and triangle area)''')
+st.markdown(r'''- $\Delta x = \frac{1}{2} (v_0 + v_t) t \quad$ (trapezoid area)''')
+st.markdown(r'''- $2 a \Delta x = (v_0 + v_t) a t = (v_t + v_0) (v_t - v_0)= {v_t}^2 - {v_0}^2\quad$ (substritute $t = (v_t - v_0)/a$)''')

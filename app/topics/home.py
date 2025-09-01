9d1803fbdc8df071435188ed9f9edfc8e73b5d4c
Jess-dudu <jessica.dudu.chen@gmail.com>
2025-09-01 19:31:49 +0000

    Minor change

--- app/topics/home.py
+++ app/topics/home.py
@@ -6,7 +6,7 @@ st.write('''
          This project started with IPython Notebook on Google Colab to help demonstrating 
          and understanding Math topics with interactive graphing, visualization, data 
          analysis & simulation. For better accessiblity and interaction, it is 
-         switched to utilize streamlit and deployed onto two free cloud servers:  \n
+         switched to utilize streamlit and deployed onto two free cloud servers:
 
          - HuggingFace Cloud Server (so far, apps respond faster):  
            - https://huggingface.co/spaces/Jess-dudu/IntuitiveMath
@@ -19,6 +19,7 @@ st.write("\n")
 st.write('''
          This project is still under development, and adding new topics. 
          Please feel free to contribute to the project by submitting issues 
-         or pull requests on GitHub repository: \n
+         or pull requests on GitHub repository:
+         
          - https://github.com/Jess-dudu/PythonForIntuitiveMath
          ''')
